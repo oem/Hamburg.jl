227e056ab4e1cbcab730762aa68d6bdb9c98caf5
Ömür Özkir <oemuer.oezkir@gmail.com>
2020-07-26 22:13:10 +0200

    we can actually get the general infection stats (total, recovered, new). This is just bursting some code, we will add proper error handling/edge cases and tests in a bit

--- Covid19.jl
+++ Covid19.jl
@@ -9,15 +9,20 @@ const URL = "https://www.hamburg.de/corona-zahlen/"
 
 function fetchcurrent()
   response = HTTP.get(URL)
-  html = Gumbo.parsehtml(String(response))
-  confirmed = parseconfirmed(html.root)
-  confirmed
+  html = parsehtml(String(response))
+  infected = parseinfected(html.root)
+  infected
 end
 
-function parseconfirmed(root)
-  el = matchFirst(sel".c_chart.one .chart_legend li", root)
-  text = el[2].text
-  parse(Int, match(r"\d+", text).match)
+function parseinfected(root)
+  infected, healed, new = map(eachmatch(sel".c_chart.one .chart_legend li", root)) do el
+    text = el[2].text
+    parse(Int, match(r"\d+", text).match)
+  end
+end
+
+function parsedeaths(root)
+  eachmatch(sel".c_chart.two .chart_legend li", root)
 end
 
 end # module
