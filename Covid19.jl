cb45f8fd8177734719b7f8279365ebe013426bcd
Ömür Özkir <oemuer.oezkir@gmail.com>
2020-07-27 09:30:37 +0200

    covid-19: parsing last 7 days trend and hospitalizations

--- Covid19.jl
+++ Covid19.jl
@@ -12,8 +12,12 @@ function fetchcurrent()
   html = parsehtml(String(response))
   infected = parseinfected(html.root)
   deaths = parsedeaths(html.root)
+  hospitalizations = parsehospitalizations(html.root)
+  trend = parsetrend(html.root)
   @show infected
   @show deaths
+  @show hospitalizations
+  @show trend
 end
 
 function parseinfected(root)
@@ -21,10 +25,19 @@ function parseinfected(root)
 end
 
 function parsedeaths(root)
-  map(parsenumbers,eachmatch(sel".c_chart.two .chart_legend li:not(.c_chart_show_noshow)", root))
+  map(parsenumbers, eachmatch(sel".c_chart.two .chart_legend li:not(.c_chart_show_noshow)", root))
+end
+
+function parsehospitalizations(root)
+  map(parsenumbers, eachmatch(sel".c_chart.three .chart_legend li", root))
+end
+
+function parsetrend(root)
+  map(el -> parse(Int, el[1].text), eachmatch(sel".cv_chart_container .value_show", root))
 end
 
 function parsenumbers(el)
+  @show el
   text = el[2].text
   parse(Int, match(r"\d+", text).match)
 end
