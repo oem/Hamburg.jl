cdc013322549284486ad565b737587caae1b16e7
Ömür Özkir <oemuer.oezkir@gmail.com>
2020-07-29 19:10:33 +0200

    updates csv loading to avoid deprecation warning

--- src/Hamburg.jl
+++ src/Hamburg.jl
@@ -1,12 +1,12 @@
 module Hamburg
 
-using CSV
+using CSV, DataFrames
 export dataset
 
 include("covid-19/Covid19.jl")
 
 function dataset(topic, dataset)
-  CSV.read("src/$topic/$dataset.csv")
+    CSV.read("src/$topic/$dataset.csv", DataFrame)
 end
 
 end # module
