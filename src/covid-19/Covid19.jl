01d4f6d8b490d3e8783758aabd3be4423b0100e4
Ömür Özkir <oemuer.oezkir@gmail.com>
2020-07-29 17:25:44 +0200

    adds CSV dependency that was used already

--- src/covid-19/Covid19.jl
+++ src/covid-19/Covid19.jl
@@ -1,6 +1,6 @@
 module Covid19
 
-using HTTP, Gumbo, Cascadia, DataFrames, CSV
+using HTTP, Gumbo, CSV, Cascadia, DataFrames
 using Cascadia: matchFirst
 
 include("DatesInGerman.jl")
@@ -10,84 +10,84 @@ const CSV_INFECTED = "src/covid-19/infected.csv"
 const CSV_BOROUGHS = "src/covid-19/boroughs.csv"
 
 function fetchcurrent()
-  response = HTTP.get(URL)
-  html = parsehtml(String(response))
+    response = HTTP.get(URL)
+    html = parsehtml(String(response))
 
-  infected = parseinfected(html.root)
-  deaths = parsedeaths(html.root)
-  hospitalizations = parsehospitalizations(html.root)
-  trend = parsetrend(html.root)
-  boroughs = parseboroughs(html.root)
-  recordedat = parsedateinfected(html.root)
+    infected = parseinfected(html.root)
+    deaths = parsedeaths(html.root)
+    hospitalizations = parsehospitalizations(html.root)
+    trend = parsetrend(html.root)
+    boroughs = parseboroughs(html.root)
+    recordedat = parsedateinfected(html.root)
 
-  Dict(:infected => Dict(:total => infected[1], :recovered => infected[2], :new => infected[3], :recordedat => recordedat),
+    Dict(:infected => Dict(:total => infected[1], :recovered => infected[2], :new => infected[3], :recordedat => recordedat),
        :deaths => Dict(:total => deaths[1], :new => deaths[2]),
        :trend => trend,
        :boroughs => boroughs)
 end
 
 function parseinfected(root)
-  map(parsenumbers, eachmatch(sel".c_chart.one .chart_legend li", root))
+    map(parsenumbers, eachmatch(sel".c_chart.one .chart_legend li", root))
 end
 
 function parsedateinfected(root)
-  daterecorded = matchFirst(sel".chart_publication", root)[1].text
-  DatesInGerman.parsefrom(daterecorded)
+    daterecorded = matchFirst(sel".chart_publication", root)[1].text
+    DatesInGerman.parsefrom(daterecorded)
 end
 
 function parsedateboroughs(root)
-  daterecorded = eachmatch(sel".table-article + p", root)[end][1].text
-  DatesInGerman.parsefrom(daterecorded)
+    daterecorded = eachmatch(sel".table-article + p", root)[end][1].text
+    DatesInGerman.parsefrom(daterecorded)
 end
 
 function parsedeaths(root)
-  map(parsenumbers, eachmatch(sel".c_chart.two .chart_legend li:not(.c_chart_show_noshow)", root))
+    map(parsenumbers, eachmatch(sel".c_chart.two .chart_legend li:not(.c_chart_show_noshow)", root))
 end
 
 function parsehospitalizations(root)
-  map(parsenumbers, eachmatch(sel".c_chart.three .chart_legend li", root))
+    map(parsenumbers, eachmatch(sel".c_chart.three .chart_legend li", root))
 end
 
 function parsetrend(root)
-  map(el -> parse(Int, el[1].text), eachmatch(sel".cv_chart_container .value_show", root))
+    map(el -> parse(Int, el[1].text), eachmatch(sel".cv_chart_container .value_show", root))
 end
 
 function parseboroughs(root)
-  rows = eachmatch(sel".table-article tr", root)[8:end]
-  mapped = Dict{String, Any}()
-  foreach(rows) do row
-    name = matchFirst(sel"td:first-child", row)[1].text
-    num = parse(Int, matchFirst(sel"td:last-child", row)[1].text)
-    mapped[name] = num
-  end
-  mapped["recordedat"] = parsedateboroughs(root)
-  mapped
+    rows = eachmatch(sel".table-article tr", root)[8:end]
+    mapped = Dict{String,Any}()
+    foreach(rows) do row
+        name = matchFirst(sel"td:first-child", row)[1].text
+        num = parse(Int, matchFirst(sel"td:last-child", row)[1].text)
+        mapped[name] = num
+    end
+    mapped["recordedat"] = parsedateboroughs(root)
+    mapped
 end
 
 function parsenumbers(el)
-  text = el[2].text
-  parse(Int, match(r"\d+", text).match)
+    text = el[2].text
+    parse(Int, match(r"\d+", text).match)
 end
 
 function record()
-  current = fetchcurrent()
-  recordInfected(current)
-  recordByBorough(current)
-  return
+    current = fetchcurrent()
+    recordInfected(current)
+    recordByBorough(current)
+    return
 end
 
 function recordInfected(current)
-  infected = current[:infected]
-  infected[:deaths] = current[:deaths][:total]
-  df = DataFrame(infected)
-  persisted = CSV.read(CSV_INFECTED)
-  unique(vcat(df, persisted), :recordedat) |> CSV.write(CSV_INFECTED)
+    infected = current[:infected]
+    infected[:deaths] = current[:deaths][:total]
+    df = DataFrame(infected)
+    persisted = CSV.read(CSV_INFECTED)
+    unique(vcat(df, persisted), :recordedat) |> CSV.write(CSV_INFECTED)
 end
 
 function recordByBorough(current)
-  df = DataFrame(current[:boroughs])
-  persisted = CSV.read(CSV_BOROUGHS)
-  unique(vcat(df, persisted), :recordedat) |> CSV.write(CSV_BOROUGHS)
+    df = DataFrame(current[:boroughs])
+    persisted = CSV.read(CSV_BOROUGHS)
+    unique(vcat(df, persisted), :recordedat) |> CSV.write(CSV_BOROUGHS)
 end
 
 end # module
