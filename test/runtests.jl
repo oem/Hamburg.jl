f14520da99627cd03646023f2ffcc4560754943d
Ömür Özkir <oemuer.oezkir@gmail.com>
2020-09-12 21:29:32 +0200

    adds some basic test coverage

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,15 @@
 using Hamburg
+using DataFrames
 using Test
 
+@testset "using `dataset()` to load datasets" begin
+    @testset "loads existing datasets into a DataFrame" begin
+        df = dataset("covid-19", "infected")
+        @test isa(df, DataFrame)
+        @test size(df)[1] > 0
+    end
+
+    @testset "throws an error when trying to load a non-existent dataset" begin
+        @test_throws ArgumentError dataset("moo", "foo")
+    end
+end
