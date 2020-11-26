afd6b4ce6412e389e89e7ce441d9c85bfde405c7
Ömür Özkir <oemuer.oezkir@gmail.com>
2020-11-26 13:14:16 +0100

    cleans up the date parsing code a little bit

--- src/covid-19/DatesInGerman.jl
+++ src/covid-19/DatesInGerman.jl
@@ -5,17 +5,21 @@ using Dates
 const MONTHS = ("januar", "februar", "märz", "april", "mai", "juni", "juli", "august", "september", "oktober", "november", "dezember")
 
 function parsefrom(date::String; inwords::Bool=true)::Date
-    if inwords
-        parts = match(r"(\d+).\s*(\S+)\s*(\d{4})", date).captures
-        Date(parse(Int, parts[3]),
+    inwords ? date |> fromwords : date |> fromnumbers
+end
+
+function fromwords(date::String)::Date
+    parts = match(r"(\d+).\s*(\S+)\s*(\d{4})", date).captures
+    Date(parse(Int, parts[3]),
            findfirst(m -> m == lowercase(parts[2]), MONTHS),
            parse(Int, parts[1]))
-    else
-        parts = match(r"(\d+)\.(\d+)\.(\d+)", date).captures
-        Date(parse(Int, "20$(parts[3])"),
+end
+
+function fromnumbers(date::String)::Date
+    parts = match(r"(\d+)\.(\d+)\.(\d+)", date).captures
+    Date(parse(Int, "20$(parts[3])"),
              parse(Int, parts[2]),
              parse(Int, parts[1]))
-    end
 end
 
 end # module
