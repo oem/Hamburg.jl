c69fd605781b2e8661afdbf3df1a5925fcbc3c66
Ömür Özkir <oemuer.oezkir@gmail.com>
2021-10-05 21:08:28 +0200

    further finetunes the public interface

--- src/Hamburg.jl
+++ src/Hamburg.jl
@@ -13,15 +13,20 @@ Takes topic and name of the specific dataset and loads it into a DataFrame.
 
 # Examples
 ```julia-repl
-julia> dataset("covid-19", "infected")
+julia> dataset("covid-19", :infected)
 ```
 
 ```julia-repl
-julia> dataset("covid-19", "infected", fetch = true)
+julia> dataset("covid-19", :infected, fetch = true)
 ```
 """
-function dataset(topic::String, dataset::String; fetch::Bool = true)::DataFrame
+function dataset(topic::String, dataset::Symbol; fetch::Bool = false)::DataFrame
     if fetch
+        if topic == "covid-19"
+            Covid19.fetch() |> Covid19.parse |> Covid19.build
+        else
+            throw(ArgumentError("live-fetching for $topic/$dataset is currently not supported. But go ahead and open an issue on github if you need this, always happy to hear from you!"))
+        end
     else
         CSV.read(joinpath(@__DIR__, topic, "$dataset.csv"), DataFrame)
     end
